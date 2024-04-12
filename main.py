078ef55efc3b3bb5cd26bb097d630407ab954391
Alvin Yan <alvinyan12345@gmail.com>
2024-04-12 05:00:48 -0400

    added basic random map generation

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import pygame, sys, time, math
+from random import random
 pygame.init()
+clock = pygame.time.Clock()
 
-swidth = 1080
-sheight = 720
+swidth = 960
+sheight = 640
 screen = pygame.display.set_mode((swidth,sheight))
 
 class SpriteSheet():
@@ -17,27 +19,54 @@ class SpriteSheet():
 
 		return image
 
-playerSpriteW = 24
-playerSpriteH = 24
+playerBaseDim = 24
+playerScale = 4
 # right facing player sprites
 playerSpriteImg = pygame.image.load("doux.png").convert_alpha()
 playerSpriteSheet = SpriteSheet(playerSpriteImg)
 
-playerFrames = list()
+playerFrames = []
 for x in range(0,23):
-    playerFrames.append(playerSpriteSheet.get_image(x, playerSpriteW, playerSpriteH, 3, (0,0,0)))
+    playerFrames.append(
+        playerSpriteSheet.get_image(
+            x, playerBaseDim, playerBaseDim, playerScale, (0,0,0)
+        )
+    )
 
 # left facing player sprites (inverted)
 playerSpriteInv = pygame.transform.flip(playerSpriteImg, True, False)
 playerSpriteInvSheet = SpriteSheet(playerSpriteInv)
 
-playerInvFrames = list()
+playerInvFrames = []
 for x in range(23,-1,-1):
-    playerInvFrames.append(playerSpriteInvSheet.get_image(x, playerSpriteW, playerSpriteH, 3, (0,0,0)))
+    playerInvFrames.append(
+        playerSpriteInvSheet.get_image(
+            x, playerBaseDim, playerBaseDim, playerScale, (0,0,0)
+        )
+    )
+
+tileScale = int((3/2)*playerScale)
+tileDim = 16
+tileSprites = []
+tileSprites.append(pygame.image.load("MapAssets/Tiles/tile_0000.png"))
+tileSprites.append(pygame.image.load("MapAssets/Tiles/tile_0001.png"))
+tileSprites.append(pygame.image.load("MapAssets/Tiles/tile_0002.png"))
+tileList = []
+for x in range(3):
+    tileList.append(SpriteSheet(tileSprites[x]).get_image(0, tileDim, tileDim, tileScale, (0,0,0)))
+
+seedMap = []
+for i in range(0,swidth+1, tileDim*tileScale):
+    randMapIndex = []
+    for j in range(0,sheight+1, tileDim*tileScale):
+        rng = int(random()*3)
+        randMapIndex.append(rng)
+    seedMap.append(randMapIndex)     
+   
 
 # player location indices
-ppx = swidth/2-playerSpriteW*2
-ppy = sheight/2-playerSpriteH*2
+ppx = swidth/2-playerBaseDim*2
+ppy = sheight/2-playerBaseDim*2
 pinitx = ppx
 pinity = ppy
 # walking animation control var
@@ -46,8 +75,7 @@ walkingInterim = walkingFrame
 playerFacingR = True
 
 # general speed value
-spd = 200
-
+spd = 250
 previousTime = time.time()
 
 while True:
@@ -56,6 +84,11 @@ while True:
     previousTime = time.time()
 
     screen.fill((0,0,0))
+    for i in range(0,swidth+1, tileDim*tileScale):
+        for j in range(0,sheight+1, tileDim*tileScale):
+            x = int(i/(tileDim*tileScale))
+            y = int(j/(tileDim*tileScale))
+            screen.blit(tileList[seedMap[x][y]], (i,j))
 
     key = pygame.key.get_pressed()
     totKeys = sum(key)
@@ -112,17 +145,20 @@ while True:
     if key[pygame.K_a] and ppx >= 150:
         playerFacingR = False
         ppx += -spd * dt * spdMultiplier
-    if key[pygame.K_d] and ppx <= swidth-playerSpriteW*2-150:
+    if key[pygame.K_d] and ppx <= swidth-150-playerBaseDim*playerScale:
         playerFacingR = True
         ppx += spd * dt * spdMultiplier
     if key[pygame.K_w] and ppy >= 150:
         ppy += -spd * dt * spdMultiplier
-    if key[pygame.K_s] and ppy <= sheight-playerSpriteH*2-150:
+    if key[pygame.K_s] and ppy <= sheight-150-playerBaseDim*playerScale:
         ppy += spd * dt * spdMultiplier
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+
+    clock.tick()
+    print(clock.get_fps())
 
     pygame.display.update()
