3a2102f0578a6ad19e721e491abd441e1b2edab2
Alvin Yan <alvinyan12345@gmail.com>
2024-04-12 14:26:21 -0400

    add basic map editing

---
+++ map.py
@@ -0,0 +1,21 @@
+# transpose map matrix for compatibility
+
+def transpose(l1, l2):
+    for i in range(len(l1[0])):
+        # print(i)
+        row =[]
+        for item in l1:
+            row.append(item[i])
+        l2.append(row)
+    return l2
+
+seedMapOG = [[2,1,2,0,2,1,0,2,1,2,0],
+             [1,1,1,0,2,1,0,2,2,0,0],
+             [6,6,8,7,0,2,1,0,0,1,1],
+             [1,2,2,8,2,0,6,8,7,6,7],
+             [0,0,0,6,8,7,9,2,2,0,1],
+             [0,2,1,0,0,1,0,1,2,2,2],
+             [0,0,1,2,1,2,1,0,1,1,1]]
+
+seedMap = []
+seedMap = transpose(seedMapOG, seedMap)
